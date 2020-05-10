b5702bdf6473eabd55e85dc7b22e07d9c1425e74
Данил Мищенко <danilmisenko@MacBook-Pro-Danil.local>
2020-05-11 00:12:08 +0300

    fixed

--- src/draw.c
+++ src/draw.c
@@ -26,13 +26,15 @@ void	zoom(float *x, float *y, int *z, t_fdf *fdf)
 	*z *= fdf->z_scale;
 }
 
-void	draw_line(float x, float y, float x1, float y1, t_fdf *fdf)
+void	draw_line_x(float x, float y, float x1, t_fdf *fdf)
 {
 	float	y_step;
 	float	x_step;
 	int		z;
 	int		z1;
+	float y1;
 
+    y1 = y;
 	zoom(&x, &y, &z, fdf);
 	zoom(&x1, &y1, &z1, fdf);
 	fdf->color = (z || z1) ? 0xe80c0c : 0xffffff;
@@ -54,6 +56,36 @@ void	draw_line(float x, float y, float x1, float y1, t_fdf *fdf)
 	}
 }
 
+void	draw_line_y(float x, float y, float y1, t_fdf *fdf)
+{
+    float	y_step;
+    float	x_step;
+    int		z;
+    int		z1;
+    float x1;
+
+    x1 = x;
+    zoom(&x, &y, &z, fdf);
+    zoom(&x1, &y1, &z1, fdf);
+    fdf->color = (z || z1) ? 0xe80c0c : 0xffffff;
+    if (fdf->iso > 0)
+    {
+        isometric(&x, &y, z, fdf);
+        isometric(&x1, &y1, z1, fdf);
+    }
+    shift(&x, &y, fdf);
+    shift(&x1, &y1, fdf);
+    y_step = y1 - y;
+    x_step = x1 - x;
+    get_step(&x_step, &y_step);
+    while ((int)(x1 - x) || (int)(y1 - y))
+    {
+        mlx_pixel_put(fdf->mlx_ptr, fdf->win_ptr, (int)x, (int)y, fdf->color);
+        x += x_step;
+        y += y_step;
+    }
+}
+
 void	draw_map(t_fdf *fdf)
 {
 	int x;
@@ -66,9 +98,9 @@ void	draw_map(t_fdf *fdf)
 		while (x < fdf->x)
 		{
 			if (x < fdf->x - 1)
-				draw_line((float)x, (float)y, (float)(x + 1), (float)y, fdf);
+				draw_line_x((float)x, (float)y, (float)(x + 1), fdf);
 			if (y < fdf->y - 1)
-				draw_line((float)x, (float)y, (float)x, (float)(y + 1), fdf);
+				draw_line_y((float)x, (float)y, (float)(y + 1), fdf);
 			x++;
 		}
 		y++;
